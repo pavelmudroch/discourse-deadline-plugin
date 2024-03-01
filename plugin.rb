5dc40fcdb68756a94864aca93c42a5d1801accfe
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-03-01 11:18:55 +0100

    release(0.4.1): patch realease

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-topic-deadline
 # about: Simple plugin to add a deadline to a topic
-# version: 0.4.0
+# version: 0.4.1
 # authors: Pavel Mudroch
 # url: https://github.com/pavelmudroch/discourse-topic-deadline
 
